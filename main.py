39d11e4bb8cf6d364c9cd815550bc4b42d4f444d
shresth19 <129366933+shresth19@users.noreply.github.com>
2024-02-07 16:32:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from unittest import main
+
+# Test your function by calling it here
+demographic_data_analyzer.calculate_demographic_data()
+
+# Run unit tests automatically
+main(module='test_module', exit=False)
